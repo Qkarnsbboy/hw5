10438935b359dac9cbb6d4fb09ebd8b9fd09f869
Quincy Karns <qkarns@usc.edu>
2023-04-11 18:10:20 +0000

    finished!

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+//checkAvail
+bool checkAvail(size_t day, size_t worker, const AvailabilityMatrix& avail);
+
+//checkShifts
+bool checkShifts(unsigned int worker, std::map<size_t, size_t>& shiftsMap);
+
+//schedSolve
+bool schedSolve(size_t row, size_t col, const size_t dailyNeed, std::map<size_t, size_t>& shiftsMap, const AvailabilityMatrix& avail, DailySchedule& sched);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +47,91 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    
+
+    //1. initialize sched to empty
+    vector<Worker_T> emptyVec(dailyNeed, INVALID_ID);
+    vector<vector<Worker_T>> temp (avail.size(), emptyVec);
+    sched = temp;
+
+    //2. create shifts map
+    std::map<size_t, size_t> shiftsMap;
+    for(size_t i = 0; i < avail[0].size(); i++){
+        shiftsMap[i] = maxShifts;
+    }
+
+    //3. call recursive function
+    bool result = schedSolve(0, 0, dailyNeed, shiftsMap, avail, sched);
+
+    //4. return either true or false
+    return result;
+
+}
+
+//schedSolve
+/*
+
+    Helper function that fills in sched and returns true if possible solution, false if no possible solution
+
+*/
+bool schedSolve(size_t row, size_t col, const size_t dailyNeed, std::map<size_t, size_t>& shiftsMap, const AvailabilityMatrix& avail, DailySchedule& sched){
+
+    //1. Base Case --> row goes past #days = avail.size()
+    if(row >= avail.size()){
+        return true;
+    }
+    //2. end of row
+    else if(col >= dailyNeed){
+        //row goes down 1 (starts at beginning of row)
+        return schedSolve(row+1, 0, dailyNeed, shiftsMap, avail, sched);
+    }
+    //3. if index has not been filled
+    else if(sched[row][col] == INVALID_ID){
+        //A. iterate through workers
+        for(unsigned int worker = 0; worker < avail[0].size(); worker++){
+            //set current spot to current worker
+            sched[row][col] = worker;
+
+            //check if valid
+            if(checkAvail(row, worker, avail) && checkShifts(worker, shiftsMap)){
+                //1. decrement map
+                shiftsMap[worker]--;
+
+                //2. check next step
+                if(schedSolve(row, col+1, dailyNeed, shiftsMap, avail, sched)){
+                    return true;
+                }
+
+                //3. reincrement map to backtrack
+                shiftsMap[worker]++;
+            }
+        }
+
+        //backtrack if can't find solution
+        sched[row][col] = INVALID_ID;
+
+        //return false cause invalid branch
+        return false;
+    }
+
+    //return false if end is reached
+    return false;
+
+}
 
 
+//checkAvail
+bool checkAvail(size_t day, size_t worker, const AvailabilityMatrix& avail){
+    return avail[day][worker];
+}
 
+//checkShifts
+bool checkShifts(unsigned int worker, std::map<size_t, size_t>& shiftsMap){
+    if(shiftsMap[worker] == 0){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void generateStrings(string& word, int index, int size, int numberSpaces, int floatingSize, std::map<char,int>& floatingMap, std::set<std::string>& stringSet, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +23,131 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    //Declare useful data structures and variables
+
+    //Declare results set
+    std::set<std::string> results;
+
+    //create floating map
+    std::map<char,int> floatingMap;
+
+    //populate map
+    for(unsigned int i = 0; i < floating.size(); i++){
+        floatingMap[floating[i]]++;
+    }
+
+    //get size
+    int size = in.length();
+
+    //floatingSize
+    int floatingSize = floating.length();
+
+    //word
+    std::string word = in;
+
+    int numberSpaces = 0;
+
+    //numberSpaces
+    for(int i = 0; i < size; i++){
+        if(word[i] == '-'){
+            numberSpaces++;
+        }
+    }
+
+    // //set word
+    // for(int i = 0; i < in.length()-1; i++)
+    // {
+    //     if(in[i] != '_'){
+    //         //fixed letter
+    //         word += in[i];
+    //     }
+    //     else{
+    //         //blank space
+    //         word += '_';
+    //     }
+    // }
+
+    //generate substrings
+    generateStrings(word, 0, size, numberSpaces, floatingSize, floatingMap, results, dict);
+
+        //check if substrings are valid
+        //if at least 1 of each floating letters are present
+
+    //check if substrings are in dictionary
+        //if valid --> keep in results set
+
+    return results;
+
 }
 
 // Define any helper functions here
+
+//generateString
+void generateStrings(string& word, int index, int size, int numberSpaces, int floatingSize, std::map<char,int>& floatingMap, std::set<std::string>& stringSet, const std::set<std::string>& dict){
+    //BC
+    if(numberSpaces < floatingSize)
+        return;
+    if(index >= size){
+        if(dict.find(word) != dict.end()){
+            stringSet.insert(word);
+        }
+        return;
+    }
+    //check empty
+    else if(word[index] != '-'){
+        // word[index] = fixed[index];
+        //recurse and increment index (don't change value)
+        generateStrings(word, index+1, size, numberSpaces, floatingSize, floatingMap, stringSet, dict);
+    }
+    else{
+        //if empty
+
+        //1. try floating first
+        auto copy = floatingMap;
+        for(auto p: floatingMap){
+            char curr = p.first;
+            if(p.second <= 0){
+                continue;
+            }
+            
+            word[index] = curr;
+            copy[curr]--;
+            //recurses down this substring
+            
+            generateStrings(word, index+1, size, numberSpaces-1, floatingSize-1, copy, stringSet, dict);
+            //backtracks
+            word[index] = '-';
+            copy[curr]++;
+            copy = floatingMap;
+        }
+
+        if(numberSpaces > floatingSize){
+             //2. check rest of alphabet
+            for(char i = 'a'; i <= 'z'; i++){
+                if(floatingMap.find(i) != floatingMap.end() && floatingMap[i] > 0)
+                    continue;
+                word[index] = i;
+                //recurses down this substring
+                generateStrings(word, index+1, size, numberSpaces-1, floatingSize, floatingMap, stringSet, dict);
+                //backtracks
+                word[index] = '-';
+               
+            }
+        }
+
+        // word[index = 'a'];
+        // return generateStrings(word, index++, size, stringSet, dict);
+        // //end of alphabet
+        // else if(word[index] == 'z'){
+        //     return generateStrings(word, index++, size, stringSet, dict);
+        // }
+        // //next letter
+        // else {
+        //     word[index] = word[index]++;
+        //     return generateStrings(word, index++, size, stringSet, dict);
+        // }
+        
+    }
+    return;
+
+}
